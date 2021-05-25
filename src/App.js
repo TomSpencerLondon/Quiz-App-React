07566a95cf8f6c33493f3eadc178fca75d8ccbbf
Tom <thomas.spencer@codurance.com>
2021-05-25 21:36:32 +0100

    add grid

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const API_URL =
   'https://opentb.com/api.php?amount=10&category=14&difficulty=easy';
 
 function App() {
+  const [questions, setQuestions] = useState();
   return <div className='container'>
-    <div className='bg-white text-purple-800 p-10 rounded-lg shadow-md'>
+    <div className='bg-white text-purple-800 p-10 rounded-lg shadow-md w-full'>
       <h2 className='text-2xl'>
-      This is where we're going to have the question.
+      1. This is where we're going to have the question?
       </h2>
     </div>
 
-    <div className='flex flex-wrap mt-4 justify-around'>
-    <button className='bg-white w-5/12 p-4 text-purple-800 font-semibold rounded shadow mb-4'>Answer 1</button>
-    <button className='bg-white w-5/12 p-4 text-purple-800 font-semibold rounded shadow mb-4'>Answer 2</button>
-    <button className='bg-white w-5/12 p-4 text-purple-800 font-semibold rounded shadow'>Answer 3</button>
-    <button className='bg-white w-5/12 p-4 text-purple-800 font-semibold rounded shadow'>Answer 4</button>
+    <div className='grid grid-cols-2 gap-6 mt-4'>
+    <button className='bg-white p-4 text-purple-800 font-semibold rounded shadow'>Answer 1</button>
+    <button className='bg-white p-4 text-purple-800 font-semibold rounded shadow'>Answer 2</button>
+    <button className='bg-white p-4 text-purple-800 font-semibold rounded shadow'>Answer 3</button>
+    <button className='bg-white p-4 text-purple-800 font-semibold rounded shadow'>Answer 4</button>
     </div>
   </div>
 }

ed12102c498c3f6eeba1bbccb04552c79e096869
Tom <thomas.spencer@codurance.com>
2021-05-25 22:28:26 +0100

    add component for questionnaire

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Questionnaire } from './components';
 
 const API_URL =
   'https://opentdb.com/api.php?amount=10&category=14&difficulty=easy';
@@ -15,21 +16,9 @@ function App() {
   }, []);
 
   return questions.length > 0 ? (<div className='container'>
-    <div className='bg-white text-purple-800 p-10 rounded-lg shadow-md w-full'>
-      <h2
-        className='text-2xl'
-        dangerouslySetInnerHTML={{ __html: questions[0].question }}
-      >
-      </h2>
-    </div>
-
-    <div className='grid grid-cols-2 gap-6 mt-4'>
-    <button className='bg-white p-4 text-purple-800 font-semibold rounded shadow'>{questions[0].correct_answer}</button>
-    <button className='bg-white p-4 text-purple-800 font-semibold rounded shadow'>{questions[0].incorrect_answers[0]}</button>
-    <button className='bg-white p-4 text-purple-800 font-semibold rounded shadow'>{questions[0].incorrect_answers[1]}</button>
-    <button className='bg-white p-4 text-purple-800 font-semibold rounded shadow'>{questions[0].incorrect_answers[2]}</button>
-    </div>
+    <Questionnaire data={questions[0]} />
   </div>) : <h2 className='text-2xl text-white font-bold'>Loading...</h2>
+
 }
 
 export default App;
